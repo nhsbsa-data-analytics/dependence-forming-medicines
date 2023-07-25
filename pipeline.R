4b6cbbe25b17f88de6fd6d78ebfb29309a13c8fb
GPlatten <32270079+GPlatten@users.noreply.github.com>
2023-07-25 15:37:15 +0100

    Add pipeline and config

---
+++ pipeline.R
@@ -0,0 +1,105 @@
+### Pipeline to run PCA annual publication
+# clear environment
+rm(list = ls())
+
+# source functions
+# this is only a temporary step until all functions are built into packages
+source("./functions/functions.R")
+
+# 1. Setup --------------------------------------------
+# load GITHUB_KEY if available in environment or enter if not
+
+if (Sys.getenv("GITHUB_PAT") == "") {
+  usethis::edit_r_environ()
+  stop(
+    "You need to set your GITHUB_PAT = YOUR PAT KEY in the .Renviron file which pops up. Please restart your R Studio after this and re-run the pipeline."
+  )
+}
+
+# load GITHUB_KEY if available in environment or enter if not
+
+if (Sys.getenv("DB_DWCP_USERNAME") == "") {
+  usethis::edit_r_environ()
+  stop(
+    "You need to set your DB_DWCP_USERNAME = YOUR DWCP USERNAME and  DB_DWCP_PASSWORD = YOUR DWCP PASSWORD in the .Renviron file which pops up. Please restart your R Studio after this and re-run the pipeline."
+  )
+}
+
+#install nhsbsaUtils package first as need check_and_install_packages()
+devtools::install_github("nhsbsa-data-analytics/nhsbsaUtils",
+                         auth_token = Sys.getenv("GITHUB_PAT"))
+
+library(nhsbsaUtils)
+
+#install and library packages
+req_pkgs <-
+  c(
+    "dplyr",
+    "stringr",
+    "data.table",
+    "yaml",
+    "openxlsx",
+    "rmarkdown",
+    "logr",
+    "highcharter",
+    "lubridate",
+    "dbplyr",
+    "tidyr",
+    "janitor",
+    "magrittr",
+    "tcltk",
+    "DT",
+    "htmltools",
+    "geojsonsf",
+    "readxl",
+    "nhsbsa-data-analytics/nhsbsaR",
+    "nhsbsa-data-analytics/nhsbsaExternalData",
+    "nhsbsa-data-analytics/accessibleTables",
+    "nhsbsa-data-analytics/nhsbsaDataExtract",
+    "nhsbsa-data-analytics/nhsbsaVis"
+  )
+
+#library/install packages as required
+nhsbsaUtils::check_and_install_packages(req_pkgs)
+
+# set up logging
+lf <-
+  logr::log_open(paste0(
+    "Y:/Official Stats/DFM/log/pfd_log",
+    format(Sys.time(), "%d%m%y%H%M%S"),
+    ".log"
+  ))
+
+# load config
+config <- yaml::yaml.load_file("config.yml")
+log_print("Config loaded", hide_notes = TRUE)
+log_print(config, hide_notes = TRUE)
+
+# load options
+nhsbsaUtils::publication_options()
+log_print("Options loaded", hide_notes = TRUE)
+
+# 2. connect to DWH and pull max CY/FY  ---------
+#build connection to warehouse
+con <- nhsbsaR::con_nhsbsa(dsn = "FBS_8192k",
+                           driver = "Oracle in OraClient19Home1",
+                           "DWCP")
+
+
+
+# 7. create markdowns -------
+
+# rmarkdown::render("pfd-narrative.Rmd",
+#                   output_format = "html_document",
+#                   output_file = "outputs/pfd_summary_narrative_2022_23_v001.html")
+# 
+# rmarkdown::render("pfd-narrative.Rmd",
+#                   output_format = "word_document",
+#                   output_file = "outputs/pfd_summary_narrative_2022_23_v001.docx")
+
+# 8. disconnect from DWH  ---------
+DBI::dbDisconnect(con)
+log_print("Disconnected from DWH", hide_notes = TRUE)
+
+#close log
+logr::log_close()
